ceceb7a2cf82381fcf1cd6b1af999213bab6cfc5
syuemingfang <syuemingfang@yahoo.com.tw>
2013-06-05 18:18:28 +0800

    change plan name skin -> EST Layout

--- index.php
+++ index.php
@@ -2,45 +2,52 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title></title>
+<title>EST Layout</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="css/skin-<?php echo $_GET["type"]; ?>.css" rel="stylesheet" />
+<link href="css/el-<?php echo $_GET["type"]; ?>.css" rel="stylesheet" />
+<style>
+hr{
+  border: 0px;
+  background-color: #cccccc;
+  height: 1px;
+}
+</style>
 </head>
 <body>
 <div style="position:fixed;right:0;bottom:0"><a href="#">Top▴</a> </div>
-<h1>WebSkin</h1>
-<p><a href="?type=default">default</a> | <a href="?type=Red">Red</a> | <a href="?type=Orange">Orange</a> | <a href="?type=Yellow">Yellow</a> | <a href="?type=Green">Green</a> | <a href="?type=Blue">Blue</a> | <a href="?type=Indigo">Indigo</a> | <a href="?type=Violet">Violet</a> </p>
-<p>Header: <a href="#headerMain">Main</a>/<a href="#headerSub">Sub</a> | Table: <a href="#tableMain">Main</a>/<a href="#tableSub">Sub</a> | <a href="#btn">Btn</a> | List: <a href="#listMain">Main</a>/<a href="#listSub">Sub</a> | Menu: <a href="#menuMain">Main</a>/<a href="#menuSub">Sub</a> </p>
+<h1>EST Layout</h1>
+<p><a href="?type=Default">Default</a> | <a href="?type=Red">Red</a> | <a href="?type=Orange">Orange</a> | <a href="?type=Yellow">Yellow</a> | <a href="?type=Green">Green</a> | <a href="?type=Blue">Blue</a> | <a href="?type=Indigo">Indigo</a> | <a href="?type=Violet">Violet</a> </p>
+<p>Header: <a href="#headerMain">Main</a>/<a href="#headerSub">Sub</a> | Table: <a href="#tableMain">Main</a>/<a href="#tableSub">Sub</a> | Btn</a>: <a href="#btnMain">Main</a>/<a href="#btnSub">Sub</a> | List: <a href="#listMain">Main</a>/<a href="#listSub">Sub</a> | Menu: <a href="#menuMain">Main</a>/<a href="#menuSub">Sub</a> | Block: <a href="#blockMain">Main</a>/<a href="#blockSub">Sub</a> </p>
 <a name="headerMain"></a>
-<h1>Header(Main)</h1>
+<h2>Header(Main)</h2>
 <?php
   for($i=1; $i<99; $i++){
-    echo "<small>sk_header_main$i ▾</small>";    
-    echo "<h1 class=\"sk_header_main$i\">トピックス</h1>";
+    echo "<small>el_header_main$i ▾</small>";    
+    echo "<h3 class=\"el_header_main$i\">トピックス</h3><hr />";
   }
   ?>
 <a name="headerSub"></a>
-<h1>Header(Sub)</h1>
+<h2>Header(Sub)</h2>
 <?php
   for($i=1; $i<99; $i++){
-    echo "<small>sk_header_sub$i ▾</small>";    
-    echo "<h1 class=\"sk_header_sub$i\">トピックス</h1>";
+    echo "<small>el_header_sub$i ▾</small>";    
+    echo "<h3 class=\"el_header_sub$i\">トピックス</h3><hr />";
   }
   ?>
 <a name="btn"></a>
-<h1>Btn</h1>
+<h2>Btn</h2>
 <?php
   for($i=1; $i<99; $i++){
-    echo "<small>sk_btn_$i ▾</small>";    
-    echo "<a class=\"sk_btn_$i\">検索</a>";
+    echo "<small>el_btn_$i ▾</small>";    
+    echo "<a class=\"el_btn_$i\">検索</a><hr />";
   }
   ?>
 <a name="tableMain"></a>
-<h1>Table(Main)</h1>
+<h2>Table(Main)</h2>
 <?php    
   for($i=1; $i<99; $i++){
-    echo "<small>sk_table_main$i ▾</small>";  
-    echo "<table class=\"sk_table_main$i\">
+    echo "<small>el_table_main$i ▾</small>";  
+    echo "<table class=\"el_table_main$i\">
     <thead>
       <tr>
       <th>現在の価格</th>
@@ -55,15 +62,15 @@
       <td>10</td>
       <td>中古</td>
       </tr>
-    </table>";
+    </table><hr />";
   }
   ?>
 <a name="tableSub"></a>
-<h1>Table(Sub)</h1>
+<h2>Table(Sub)</h2>
 <?php    
     for($i=1; $i<99; $i++){
-      echo "<small>sk_table_sub$i ▾</small>";   
-      echo "<table class=\"sk_table_sub$i\">
+      echo "<small>el_table_sub$i ▾</small>";   
+      echo "<table class=\"el_table_sub$i\">
       <thead>
       <tr>
       <th>現在の価格</th>
@@ -78,34 +85,54 @@
       <td>10</td>
       <td>中古</td>
       </tr>
-      </table>";
+      </table><hr />";
     }
     ?>
 <a name="listMain"></a>
-<h1>List(Main)</h1>
+<h2>List(Main)</h2>
 <?php    
     for($i=1; $i<99; $i++){
-      echo "<small>sk_list_main$i ▾</small>";   
-      echo "<ul class=\"sk_list_main$i\">
+      echo "<small>el_list_main$i ▾</small>";   
+      echo "<h3 class=\"el_header_sub1\">トピックス</h3><ul class=\"el_list_main$i\">
       <li>天気</li>
       <li>スポーツ</li>
       <li>ファイナンス</li>
       <li>テレビ</li>
-    </ul>";
+    </ul><hr />";
     }
     ?>
 <a name="listSub"></a>
-<h1>List(Sub)</h1>
+<h2>List(Sub)</h2>
 <?php    
     for($i=1; $i<99; $i++){
-      echo "<small>sk_list_sub$i ▾</small>";   
-      echo "<ul class=\"sk_list_sub$i\">
+      echo "<small>el_list_sub$i ▾</small>";   
+      echo "<ul class=\"el_list_sub$i\">
       <li>天気</li>
       <li>スポーツ</li>
       <li>ファイナンス</li>
       <li>テレビ</li>
-    </ul>";
+    </ul><hr />";
     }
     ?>
+<a name="blockMain"></a>
+<h2>Block(Main)</h2>
+<?php    
+    for($i=1; $i<99; $i++){
+      echo "<small>el_block_main$i ▾</small>";   
+      echo "<div class=\"el_block_main$i\">
+      天気 スポーツ<br />ファイナンス テレビ
+    </div><hr />";
+    }
+    ?> 
+<a name="blockSub"></a>
+<h2>Block(Sub)</h2>
+<?php    
+    for($i=1; $i<99; $i++){
+      echo "<small>el_block_sub$i ▾</small>";   
+      echo "<div class=\"el_block_sub$i\">
+      天気 スポーツ<br />ファイナンス テレビ
+    </div><hr />";
+    }
+    ?>    
 </body>
 </html>
