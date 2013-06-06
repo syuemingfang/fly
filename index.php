34f4b759a653c8dba2bdffd7e0f03a5feaca9952
syuemingfang <syuemingfang@yahoo.com.tw>
2013-06-06 18:23:14 +0800

    update13060602

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 <meta charset="utf-8">
 <title>EST Layout</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/es-<?php echo $_GET["type"]; ?>.css" rel="stylesheet" />
+<link href="css/eo.css" rel="stylesheet" />
 <link href="css/el-<?php echo $_GET["type"]; ?>.css" rel="stylesheet" />
 <style>
 hr{
@@ -14,6 +16,15 @@ hr{
 </style>
 </head>
 <body>
+  <div class="eo_polaroid">xxx</div>
+<div>
+<h1>EST</h1>
+<cite>style + order + layout</cite>
+</div>
+<h1>EST Style</h1>
+<a href="#">会社概要</a> <a href="#">利用規約</a>
+<input type="text" />
+<input type="submit" value="検索" />
 <div style="position:fixed;right:0;bottom:0"><a href="#">Top▴</a> </div>
 <h1>EST Layout</h1>
 <p><a href="?type=Default">Default</a> | <a href="?type=Red">Red</a> | <a href="?type=Orange">Orange</a> | <a href="?type=Yellow">Yellow</a> | <a href="?type=Green">Green</a> | <a href="?type=Blue">Blue</a> | <a href="?type=Indigo">Indigo</a> | <a href="?type=Violet">Violet</a> </p>
