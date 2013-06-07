1f0170fe1fdd2d7a7540411ebc1bb46a6dc78fbb
syuemingfang <syuemingfang@yahoo.com.tw>
2013-06-07 18:10:06 +0800

    change plan name EST -> Fastest

--- index.php
+++ index.php
@@ -2,54 +2,54 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>EST Layout</title>
+<title>Jet Layout</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="css/es-<?php echo $_GET["type"]; ?>.css" rel="stylesheet" />
-<link href="css/eo.css" rel="stylesheet" />
-<link href="css/el-<?php echo $_GET["type"]; ?>.css" rel="stylesheet" />
+<link href="css/fs-<?php echo $_GET["type"]; ?>.css" rel="stylesheet" />
+<link href="css/fo.css" rel="stylesheet" />
+<link href="css/fl-<?php echo $_GET["type"]; ?>.css" rel="stylesheet" />
 <link href="css/style.css" rel="stylesheet" />
 </head>
-<h1>EST</h1>
+<h1>Fastest</h1>
 <cite>style + order + layout</cite>
 </div>
-<h1>EST Style</h1>
+<h1>Fastest Style</h1>
 <a href="#">会社概要</a> <a href="#">利用規約</a>
 <input type="text" />
 <input type="submit" value="検索" />
 <div style="position:fixed;right:0;bottom:0"><a href="#">Top▴</a> </div>
-<h1>EST Layout</h1>
+<h1>Fastest Layout</h1>
 <p><a href="?type=Default">Default</a> | <a href="?type=Red">Red</a> | <a href="?type=Orange">Orange</a> | <a href="?type=Yellow">Yellow</a> | <a href="?type=Green">Green</a> | <a href="?type=Blue">Blue</a> | <a href="?type=Indigo">Indigo</a> | <a href="?type=Violet">Violet</a> </p>
 <p>Header: <a href="#headerMain">Main</a>/<a href="#headerSub">Sub</a> | Table: <a href="#tableMain">Main</a>/<a href="#tableSub">Sub</a> | Btn</a>: <a href="#btnMain">Main</a>/<a href="#btnSub">Sub</a> | List: <a href="#listMain">Main</a>/<a href="#listSub">Sub</a> | Menu: <a href="#menuMain">Main</a>/<a href="#menuSub">Sub</a> | Block: <a href="#blockMain">Main</a>/<a href="#blockSub">Sub</a> </p>
 <a name="headerMain"></a>
 <h2>Header(Main)</h2>
 <?php
   for($i=1; $i<99; $i++){
-    echo "<small>el_header_main$i ▾</small>";    
-    echo "<h3 class=\"el_header_main$i\">トピックス</h3><hr />";
+    echo "<small>fl_header_main$i ▾</small>";    
+    echo "<h3 class=\"fl_header_main$i\">トピックス</h3><hr />";
   }
   ?>
 <a name="headerSub"></a>
 <h2>Header(Sub)</h2>
 <?php
   for($i=1; $i<99; $i++){
-    echo "<small>el_header_sub$i ▾</small>";    
-    echo "<h3 class=\"el_header_sub$i\">トピックス</h3><hr />";
+    echo "<small>fl_header_sub$i ▾</small>";    
+    echo "<h3 class=\"fl_header_sub$i\">トピックス</h3><hr />";
   }
   ?>
 <a name="btn"></a>
 <h2>Btn</h2>
 <?php
   for($i=1; $i<99; $i++){
-    echo "<small>el_btn_$i ▾</small>";    
-    echo "<a class=\"el_btn_$i\">検索</a><hr />";
+    echo "<small>fl_btn_$i ▾</small>";    
+    echo "<a class=\"fl_btn_$i\">検索</a><hr />";
   }
   ?>
 <a name="tableMain"></a>
 <h2>Table(Main)</h2>
 <?php    
   for($i=1; $i<99; $i++){
-    echo "<small>el_table_main$i ▾</small>";  
-    echo "<table class=\"el_table_main$i\">
+    echo "<small>fl_table_main$i ▾</small>";  
+    echo "<table class=\"fl_table_main$i\">
     <thead>
       <tr>
       <th>現在の価格</th>
@@ -71,8 +71,8 @@
 <h2>Table(Sub)</h2>
 <?php    
     for($i=1; $i<99; $i++){
-      echo "<small>el_table_sub$i ▾</small>";   
-      echo "<table class=\"el_table_sub$i\">
+      echo "<small>fl_table_sub$i ▾</small>";   
+      echo "<table class=\"fl_table_sub$i\">
       <thead>
       <tr>
       <th>現在の価格</th>
@@ -94,8 +94,8 @@
 <h2>List(Main)</h2>
 <?php    
     for($i=1; $i<99; $i++){
-      echo "<small>el_list_main$i ▾</small>";   
-      echo "<h3 class=\"el_header_sub1\">トピックス</h3><ul class=\"el_list_main$i\">
+      echo "<small>fl_list_main$i ▾</small>";   
+      echo "<h3 class=\"fl_header_sub1\">トピックス</h3><ul class=\"fl_list_main$i\">
       <li>天気</li>
       <li>スポーツ</li>
       <li>ファイナンス</li>
@@ -107,8 +107,8 @@
 <h2>List(Sub)</h2>
 <?php    
     for($i=1; $i<99; $i++){
-      echo "<small>el_list_sub$i ▾</small>";   
-      echo "<ul class=\"el_list_sub$i\">
+      echo "<small>fl_list_sub$i ▾</small>";   
+      echo "<ul class=\"fl_list_sub$i\">
       <li>天気</li>
       <li>スポーツ</li>
       <li>ファイナンス</li>
@@ -120,8 +120,8 @@
 <h2>Block(Main)</h2>
 <?php    
     for($i=1; $i<99; $i++){
-      echo "<small>el_block_main$i ▾</small>";   
-      echo "<div class=\"el_block_main$i\">
+      echo "<small>fl_block_main$i ▾</small>";   
+      echo "<div class=\"fl_block_main$i\">
       天気 スポーツ<br />ファイナンス テレビ
     </div><hr />";
     }
@@ -130,8 +130,8 @@
 <h2>Block(Sub)</h2>
 <?php    
     for($i=1; $i<99; $i++){
-      echo "<small>el_block_sub$i ▾</small>";   
-      echo "<div class=\"el_block_sub$i\">
+      echo "<small>fl_block_sub$i ▾</small>";   
+      echo "<div class=\"fl_block_sub$i\">
       天気 スポーツ<br />ファイナンス テレビ
     </div><hr />";
     }
---
+++ perfectColor.php
@@ -0,0 +1,66 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Perfect Color</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/perfectColor.css" rel="stylesheet" /></head>
+<h1>Perfect Color</h1>
+https://kuler.adobe.com/
+<div style="clear: both">
+總覽
+</div>
+<?php
+  for($i=1; $i<9; $i++){
+    echo "<div class=\"color$i\"></div>";
+  }
+?>
+<div style="clear: both">
+Analogous 類比
+</div>
+<?php
+  for($i=1; $i<6; $i++){
+    echo "<div class=\"analogous$i\"></div>";
+  }
+?>
+<div style="clear: both">
+Monochromatic 單色
+</div>
+<?php
+  for($i=1; $i<6; $i++){
+    echo "<div class=\"monochromatic$i\"></div>";
+  }
+?>
+<div style="clear: both">
+Triad 三元素
+</div>
+<?php
+  for($i=1; $i<6; $i++){
+    echo "<div class=\"triad$i\"></div>";
+  }
+?>
+<div style="clear: both">
+Complementary 互補
+</div>
+<?php
+  for($i=1; $i<6; $i++){
+    echo "<div class=\"complementary$i\"></div>";
+  }
+?>
+<div style="clear: both">
+Compound 混合
+</div>
+<?php
+  for($i=1; $i<6; $i++){
+    echo "<div class=\"compound$i\"></div>";
+  }
+?>
+<div style="clear: both">
+Shades 明暗
+</div>
+<?php
+  for($i=1; $i<6; $i++){
+    echo "<div class=\"shades$i\"></div>";
+  }
+?>
+</body></html>
